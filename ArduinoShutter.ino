fb9badd8e7034696efa7f9dd465647c79ce57718
Jan Kuhfeld <jan@kuhfeld.net>
2025-04-12 18:11:25 +0900

    SCPI commands

--- ArduinoShutter.ino
+++ ArduinoShutter.ino
@@ -1,39 +1,121 @@
-#include <Servo.h> 
-#include <CmdBuffer.hpp>
-#include <CmdCallback.hpp>
-#include <CmdParser.hpp>
+const int VERSION = 1;
 
-CmdCallback<3> cmdCallback;
-char strSet[] = "SET";
+#include <Servo.h>
 
-Servo servo; 
+#include <Vrekrer_scpi_parser.h>
+#include <EEPROM.h>
 
 
-void functSet(CmdParser *myParser)
-{
-  int pos = atoi(myParser->getCmdParam(1));
-  Serial.println(pos);
-  servo.write(pos);
-}
+Servo servo;
 
+SCPI_Parser instrument;
 
-void setup()
-{
+volatile int open_pos = 0;
+volatile int close_pos = 90;
+
+const unsigned int open_pos_addr = 10;
+const unsigned int close_pos_addr = 20;
+int magic = 0;
+void setup() {
   Serial.begin(9600);
-  cmdCallback.addCmd(strSet, &functSet);
 
-  servo.attach(9); // connect yellow wire of servo with pin d9
+
+instrument.RegisterCommand(F("*IDN?"), &IDN);
+  instrument.SetCommandTreeBase(F("POSition"));
+  instrument.RegisterCommand(F(":SET"), &SetPosition);
+  instrument.RegisterCommand(F(":SET?"), &GetPosition);
+  instrument.RegisterCommand(F(":OPen"), &SetOpen);
+  instrument.RegisterCommand(F(":CLose"), &SetClose);
+
+
+
+  instrument.SetCommandTreeBase(F("SYSTem"));
+  // instrument.RegisterCommand(F(":DEBug"), &SetDebug);
+  // instrument.RegisterCommand(F(":RESet"), &ResetEEPROM);
+
+  // instrument.SetCommandTreeBase(F("SYSTem:POSition"));
+  instrument.RegisterCommand(F(":OPen"), &SetOpenPosition);
+  instrument.RegisterCommand(F(":OPen?"), &GetOpenPosition);
+  instrument.RegisterCommand(F(":CLose"), &SetClosePosition);
+  instrument.RegisterCommand(F(":CLose?"), &GetClosePosition);
+
+  servo.attach(2);  // connect yellow wire of servo with pin d9
+
+  EEPROM.get(0, magic);
+
+  if (magic == VERSION) {
+    EEPROM.get(open_pos_addr, open_pos);
+    EEPROM.get(close_pos_addr, close_pos);
+
+    Serial.println("Configuration loaded!");
+
+  } else {
+    EEPROM.put(open_pos_addr, open_pos);
+    EEPROM.put(close_pos_addr, close_pos);
+
+    EEPROM.put(0, VERSION);
+    Serial.println("No configuration found, set defaults!");
+  }
+
+  Serial.println("Setup completed!");
+  Serial.print("Open position: ");
+  Serial.println(open_pos);
+  Serial.print("Close position: ");
+  Serial.println(close_pos);
 }
 
- 
 
-void loop()
-{ 
-  CmdBuffer<32> myBuffer;
-  CmdParser myParser;
 
+void loop() {
   // Serial.println("To set servo position use SET <pos>.");
   // Serial.println("<pos> can be an integer between 0 and 180.");
-  
-  cmdCallback.loopCmdProcessing(&myParser, &myBuffer, &Serial);
+
+  instrument.ProcessInput(Serial, "\n");
+}
+
+
+void IDN(SCPI_C commands, SCPI_P parameters, Stream& interface) {
+  interface.println("Jan Kuhfeld,ArduinoShutter,None,0.1/0.1");
+}
+
+void SetPosition(SCPI_C commands, SCPI_P parameters, Stream& interface) {
+  if (parameters.Size() > 0) {
+    int pos = constrain(String(parameters[0]).toInt(), 0, 180);
+    servo.write(pos);
+  }
+}
+
+void SetOpen(SCPI_C commands, SCPI_P parameters, Stream& interface) {
+  servo.write(open_pos);
+}
+
+void SetClose(SCPI_C commands, SCPI_P parameters, Stream& interface) {
+  servo.write(close_pos);
+}
+
+void GetPosition(SCPI_C commands, SCPI_P parameters, Stream& interface) {
+  interface.println(servo.read());
+}
+
+void SetOpenPosition(SCPI_C commands, SCPI_P parameters, Stream& interface) {
+  if (parameters.Size() > 0) {
+    open_pos = constrain(String(parameters[0]).toInt(), 0, 180);
+    // interface.println(open_pos);
+    EEPROM.put(open_pos_addr, open_pos);
+  }
+}
+
+void GetOpenPosition(SCPI_C commands, SCPI_P parameters, Stream& interface) {
+  interface.println(open_pos);
+}
+void SetClosePosition(SCPI_C commands, SCPI_P parameters, Stream& interface) {
+  if (parameters.Size() > 0) {
+    close_pos = constrain(String(parameters[0]).toInt(), 0, 180);
+    // interface.println(close_pos);
+
+    EEPROM.put(close_pos_addr, close_pos);
+  }
+}
+void GetClosePosition(SCPI_C commands, SCPI_P parameters, Stream& interface) {
+  interface.println(close_pos);
 }
